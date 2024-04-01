1a08da273a4f70b672092e5c5f5a044455d60c42
Nikky06184 <nikky06184@gmail.com>
2024-04-01 15:18:27 +0530

    hello2

--- pushtopycharmtomysql.py
+++ pushtopycharmtomysql.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from  sqlalchemy import create_engine
 
-engine = create_engine (mysql+mysqlconnector ://root:Test1234!%@localhost/nik)
+engine = create_engine ("mysql+mysqlconnector ://root:Test1234!%@localhost/nik")
 
 table_name = nik2
-df = pd.read_csv("my2practicefile.csv", index = False)
+df1 = pd.read_csv("my2practicefile.csv", index = False)
+
+table_name = company1
+
+df1.to_sql(name = table_name,con = engin ,if_exists="replace",index =False)
+
+engine= dispose()
 
 
 
